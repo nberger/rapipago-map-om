98f6a4570d55ac4c0654a95a04b2c86d5104e603
Nicolas Berger <nicoberger@gmail.com>
2014-06-24 18:15:43 -0300

    Show province select, with dummy province

--- src/cljs/rapipago_map_om/core.cljs
+++ src/cljs/rapipago_map_om/core.cljs
@@ -4,10 +4,28 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:search-params {}
+                      :search-result {}}))
+
+(defn province-filter [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:provinces [{:name "Buenos Aires" :id "B"}]})
+    om/IWillMount
+    (will-mount [_]
+      )
+    om/IRenderState
+    (render-state [_, {:keys [provinces]}]
+      (dom/div #js {:id "province-filter"}
+           (dom/label #js {:for "province"} "Provincia")
+           (apply dom/select nil
+                   (map #(dom/option #js {:value (:id %)
+                                          :selected (= % (:province app))}
+                                     (:name %))
+                        provinces))))))
 
 (om/root
-  (fn [app owner]
-    (dom/h1 nil (:text app)))
+  province-filter
   app-state
   {:target (. js/document (getElementById "app"))})
