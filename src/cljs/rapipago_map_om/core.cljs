383b815eef25df1aa9f53de4aedc6d4c7a2f0b55
Nicolas Berger <nicoberger@gmail.com>
2014-06-25 00:45:12 -0300

    Fetch provinces with xhr

--- src/cljs/rapipago_map_om/core.cljs
+++ src/cljs/rapipago_map_om/core.cljs
@@ -1,8 +1,19 @@
 (ns rapipago-map-om.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [goog.events :as events])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
 
 (enable-console-print!)
+
+(defn json-xhr [{:keys [method url on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (on-complete (js->clj (.getResponseJson xhr) :keywordize-keys true))))
+    (. xhr (send url method))))
 
 (def app-state (atom {:search-params {}
                       :search-result {}}))
@@ -10,20 +21,20 @@
 (defn province-filter [app owner]
   (reify
     om/IInitState
-    (init-state [_]
-      {:provinces [{:name "Buenos Aires" :id "B"}]})
+    (init-state [_] {:provinces []})
     om/IWillMount
     (will-mount [_]
-      )
+      (json-xhr {:method "GET"
+                 :url "http://localhost:3001/provinces"
+                 :on-complete #(om/set-state! owner :provinces %)}))
     om/IRenderState
-    (render-state [_, {:keys [provinces]}]
+    (render-state [_ {:keys [provinces]}]
       (dom/div #js {:id "province-filter"}
-           (dom/label #js {:for "province"} "Provincia")
-           (apply dom/select nil
-                   (map #(dom/option #js {:value (:id %)
-                                          :selected (= % (:province app))}
-                                     (:name %))
-                        provinces))))))
+               (dom/label #js {:htmlFor "province"} "Provincia")
+               (apply dom/select #js {:id "province"}
+                      (map #(dom/option #js {:value (:id %)}
+                                        (:name %))
+                           provinces))))))
 
 (om/root
   province-filter
